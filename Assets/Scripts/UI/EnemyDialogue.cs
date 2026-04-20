6ec10fe2b8c06cd82cb111705e20be2e4ebc7ef2
Alexander.Daley <alexander.daley@mymail.champlain.edu>
2026-04-20 17:13:30 -0400

    NPC dialogue shows up when player gets close and just on their screen

---
+++ Assets/Scripts/UI/EnemyDialogue.cs
@@ -0,0 +1,55 @@
+using Fusion;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static GameStateHandler;
+
+public class EnemyDialogue : NetworkBehaviour
+{
+    [SerializeField] private GameObject canvas;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //canvas.SetActive(true);
+        if (other.CompareTag("Player"))
+        {
+            //canvas.SetActive(true);
+            RPC_PlayerEnter(other.GetComponent<NetworkCharacterController>().Object.InputAuthority);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //canvas.SetActive(false);
+        if (other.CompareTag("Player"))
+        {
+            //canvas.SetActive(false);
+            RPC_PlayerExit(other.GetComponent<NetworkCharacterController>().Object.InputAuthority);
+        }
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void RPC_PlayerEnter([RpcTarget] PlayerRef playerID)
+    {
+        canvas.SetActive(true);
+        
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void RPC_PlayerExit([RpcTarget] PlayerRef playerID)
+    {
+        canvas.SetActive(false);
+    }
+}
