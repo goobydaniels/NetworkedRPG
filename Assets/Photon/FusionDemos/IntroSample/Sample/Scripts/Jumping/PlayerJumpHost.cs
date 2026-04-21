8028257d587f9a266158a6d7e1ac453b9f9044d8
Alex <zander.plays.games.12@gmail.com>
2026-04-21 02:21:47 -0400

    JUMP WORKS!!!!

--- Assets/Photon/FusionDemos/IntroSample/Sample/Scripts/Jumping/PlayerJumpHost.cs
+++ Assets/Photon/FusionDemos/IntroSample/Sample/Scripts/Jumping/PlayerJumpHost.cs
@@ -10,7 +10,6 @@ namespace FusionDemo
     public class PlayerJumpHost : NetworkBehaviour
     {
         private NetworkCharacterController _cc;
-        private bool isInBattle;
 
         // Previous NetworkButtons. Used to detect if a button was pressed in the previous tick.
         private NetworkButtons _prevInputButtons;
@@ -19,21 +18,12 @@ namespace FusionDemo
         {
             // get the NetworkCharacterController reference
             _cc = GetBehaviour<NetworkCharacterController>();
-
-            if (SceneManager.GetActiveScene().name == "BattleTesting")
-            {
-                isInBattle = true;
-            }
-            else
-            {
-                isInBattle = false;
-            }
         }
 
         public override void FixedUpdateNetwork()
         {
             // Get input for this tick.
-            if (Object.HasInputAuthority && GetInput<PlayerInputAction>(out var input))
+            if (GetInput<PlayerInputAction>(out var input))
             {
                 // If the jump button was pressed.
                 if (input.buttons.WasPressed(_prevInputButtons, InputButton.JUMP))
@@ -44,12 +34,6 @@ namespace FusionDemo
                 // Store the input buttons to use on the next tick.
                 _prevInputButtons = input.buttons;
             }
-        }
-
-        private void OnDrawGizmosSelected()
-        {
-            // Draw the interact area when selected.
-            // Gizmos.DrawSphere(transform.position + transform.forward * 1.5f, _interactRadius);
         }
     }
 }
--- Assets/Photon/FusionDemos/IntroSample/Sample/Scripts/Jumping/PlayerJumpShared.cs
+++ Assets/Photon/FusionDemos/IntroSample/Sample/Scripts/Jumping/PlayerJumpShared.cs
@@ -8,55 +8,28 @@ namespace FusionDemo
 {
     public class PlayerJumpShared : NetworkBehaviour
     {
-        // NetworkCharacterController
-        private NetworkCharacterController _cc;
-        private bool isInBattle;
+        private NetworkCharacterController _controller;
 
-        // Used to detect if the jump button was pressed in shared mode context.
-        private bool _jumpPressed;
+        private NetworkButtons _prevInputButtons;
 
-        public override void Spawned()
+        private void Awake()
         {
-            // get the NetworkCharacterController reference
-            _cc = GetBehaviour<NetworkCharacterController>();
-
-            if (SceneManager.GetActiveScene().name == "BattleTesting")
-            {
-                isInBattle = true;
-            }
-            else
-            {
-                isInBattle = false;
-            }
+            _controller = GetBehaviour<NetworkCharacterController>();
         }
 
         public override void FixedUpdateNetwork()
         {
-            // If the Jump button was pressed.
-            if (GetJumpInput())
+            if (GetInput<PlayerInputAction>(out var input))
             {
-                _cc.Jump(false);
+                // If the jump button was pressed.
+                if (input.buttons.WasPressed(_prevInputButtons, InputButton.JUMP))
+                {
+                    _controller.Jump(false);
+                }
+
+                // Store the input buttons to use on the next tick.
+                _prevInputButtons = input.buttons;
             }
-            _jumpPressed = false;
-        }
-
-        #if !UNITY_IOS && !UNITY_ANDROID
-        private void Update()
-        {
-            // Detect interact input in update and store it to use it in FUN.
-            // if (Object.HasStateAuthority == false) return;
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _jumpPressed = true;
-            }
-        }
-        #endif
-
-        private bool GetJumpInput()
-        {
-            return _jumpPressed;
         }
     }
-
 }
