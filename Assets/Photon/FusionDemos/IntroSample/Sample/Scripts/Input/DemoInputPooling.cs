ad6fc3ec747c87898f5f7dc8a790e5ad567a7d8d
Alex <zander.plays.games.12@gmail.com>
2026-04-20 23:39:10 -0400

    Revert "Input polling refactor"
    This reverts commit 54864a21c29c9b1868713ba959cccdc54d0c7362.

--- Assets/Photon/FusionDemos/IntroSample/Sample/Scripts/Input/DemoInputPooling.cs
+++ Assets/Photon/FusionDemos/IntroSample/Sample/Scripts/Input/DemoInputPooling.cs
@@ -1,100 +1,40 @@
-﻿using Fusion;
-using Fusion.Menu;
-using Fusion.Sockets;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using Fusion;
+using Fusion.Sockets;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
 
 namespace FusionDemo {
-    public class DemoInputPooling : SimulationBehaviour, IBeforeUpdate, INetworkRunnerCallbacks {
-        private PlayerInputAction inputSystem;
-        private bool resetInput;
-
-        void IBeforeUpdate.BeforeUpdate()
-        {
-            if (resetInput)
-            {
-                resetInput = false;
-                inputSystem = default;
-            }
-
-            Keyboard keyboard = Keyboard.current;
-            if (keyboard != null && (keyboard.wasUpdatedThisFrame || keyboard.numpadEnterKey.wasPressedThisFrame || keyboard.escapeKey.wasPressedThisFrame))
-            {
-                if (Cursor.lockState == CursorLockMode.Locked)
-                {
-                    Cursor.lockState = CursorLockMode.None;
-                    Cursor.visible = true;
-                }
-                else
-                {
-                    Cursor.lockState = CursorLockMode.Locked;
-                    Cursor.visible = false;
-                }
-            }
-
-            if (Cursor.lockState != CursorLockMode.Locked)
-            {
-                return;
-            }
-
-            NetworkButtons buttons = default;
-
-            if (keyboard != null)
-            {
-                Vector2 moveDirection = Vector2.zero;
-
-                if (keyboard.wKey.isPressed) moveDirection += Vector2.up;
-                if (keyboard.sKey.isPressed) moveDirection += Vector2.down;
-                if (keyboard.aKey.isPressed) moveDirection += Vector2.left;
-                if (keyboard.dKey.isPressed) moveDirection += Vector2.right;
-
-                inputSystem.moveDirection += moveDirection;
-
-                buttons.Set(InputButton.INTERACT, keyboard.eKey.isPressed);
-                buttons.Set(InputButton.JUMP, keyboard.spaceKey.isPressed);
-            }
-
-            inputSystem.buttons = new NetworkButtons(inputSystem.buttons.Bits | buttons.Bits);
-        }
-        
+    public class DemoInputPooling : MonoBehaviour, INetworkRunnerCallbacks {
         // Pooling the input
-        void INetworkRunnerCallbacks.OnInput(NetworkRunner runner, NetworkInput input)
-        {
-            inputSystem.moveDirection.Normalize();
+        public void OnInput(NetworkRunner runner, NetworkInput input) {
+            PlayerInputAction inputSystem = new();
+            Keyboard keyboard = Keyboard.current;
+
+            Vector3 moveDirection = Vector3.zero;
+
+            if (keyboard.wKey.isPressed) moveDirection += Vector3.forward;
+            if (keyboard.sKey.isPressed) moveDirection += Vector3.back;
+            if (keyboard.aKey.isPressed) moveDirection += Vector3.left;
+            if (keyboard.dKey.isPressed) moveDirection += Vector3.right;
+
+            inputSystem.moveDirection = moveDirection.normalized;
+
+            inputSystem.buttons.Set(InputButton.INTERACT, keyboard.eKey.isPressed);
+            inputSystem.buttons.Set(InputButton.JUMP, keyboard.spaceKey.isPressed);
+
             input.Set(inputSystem);
-            resetInput = true;
         }
 
         #region Other callbacks
 
         public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
         public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
-        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-        {
-            if (player == runner.LocalPlayer)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-        }
+        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) { }
         public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
         public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
-        async void INetworkRunnerCallbacks.OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-
-            if (shutdownReason == ShutdownReason.DisconnectedByPluginLogic)
-            {
-                await FindFirstObjectByType<FusionMenuConnectionBehaviourSdk>(FindObjectsInactive.Include).DisconnectAsync(ConnectFailReason.Disconnect);
-                FindFirstObjectByType<FusionMenuUIGameplay>(FindObjectsInactive.Include).Controller.Show<FusionMenuUIMain>();
-            }
-        }
-
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
         public void OnConnectedToServer(NetworkRunner runner) { }
         public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
         public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
