ec064f238c7e76d7d3499ed39ce8ec7c516f80f9
Alexander.Daley <alexander.daley@mymail.champlain.edu>
2026-04-30 18:00:07 -0400

    NPC changes to add functionality for different NPC types

--- Assets/Scripts/UI/EnemyDialogue.cs
+++ Assets/Scripts/UI/EnemyDialogue.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using UnityEngine;
 using static GameStateHandler;
 
+enum NPCTYPE
+{
+    enemy = 0,
+    potion,
+    smith,
+};
+
 public class EnemyDialogue : NetworkBehaviour
 {
     [SerializeField] private GameObject canvas;
+    [SerializeField] private NPCTYPE type;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -44,12 +52,32 @@ public class EnemyDialogue : NetworkBehaviour
     public void RPC_PlayerEnter([RpcTarget] PlayerRef playerID)
     {
         canvas.SetActive(true);
-        
+        itemGive();
     }
 
     [Rpc(RpcSources.All, RpcTargets.All)]
     public void RPC_PlayerExit([RpcTarget] PlayerRef playerID)
     {
         canvas.SetActive(false);
+    }
+
+    //Function lets NPC interact with players beyond just transitioning to battle
+    private void itemGive()
+    {
+        switch(type)
+        {
+            case NPCTYPE.potion:
+                //call function to give potion
+                break;
+            case NPCTYPE.smith:
+                //call function to give player sword
+                break;
+            case NPCTYPE.enemy:
+                //nothing
+                break;
+            default:
+                //nothing
+                break;
+        }
     }
 }
